4a7c085e44d93e6aa9af0cd751b18f978e17206d
Bumindu Yasith <bumiduyasith@gmail.com>
2022-05-17 19:45:43 +0530

    add IdentifyShortestPath page

---
+++ frontend/src/Pages/IdentifyShortestPath.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef, useState } from 'react';
+import ShortestPathGraph from '../Components/ShortestPathGraph';
+
+
+function IdentifyShortestPath() {
+  
+  return (
+    <div className="App">
+
+      <ShortestPathGraph />
+      
+    </div>
+  );
+}
+
+export default IdentifyShortestPath;
