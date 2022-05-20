80eb8ef398bbe83ff8b5254225b088f7db502c31
Bumindu Yasith <bumiduyasith@gmail.com>
2022-05-20 21:25:53 +0530

    add get all solutions for eight queens

---
+++ backend/PDSACW/PDSACW.Application/Features/EightQueensGame/Queries/GetAllEightQueensDetailQuery.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PDSACW.Application.Common.Interfaces;
+using PDSACW.Application.Common.Models;
+using PDSACW.Application.ViewModels;
+using PDSACW.Domain.Entities;
+
+
+namespace PDSACW.Application.Features.EightQueensGame.Queries
+{
+    
+    public class GetAllEightQueensDetailQuery : IRequest<List<EightQueens>>
+    {
+    }
+
+    public class GetAllEightQueensDetailHandler : IRequestHandler<GetAllEightQueensDetailQuery, List<EightQueens>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetAllEightQueensDetailHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public  async Task<List<EightQueens>> Handle(GetAllEightQueensDetailQuery request, CancellationToken cancellationToken)
+        {
+
+            var data = await  _context.EightQueens.AsNoTracking().ToListAsync();
+
+            return data;
+        }
+    }
+}
--- backend/PDSACW/PDSACW.WebApi/Controllers/EightQueensController.cs
+++ backend/PDSACW/PDSACW.WebApi/Controllers/EightQueensController.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PDSACW.Application.Features.EightQueensGame.Commands;
+using PDSACW.Application.Features.EightQueensGame.Queries;
 
 namespace PDSACW.WebApi.Controllers
 {
    
     public class EightQueensController : BaseApiController
     {
-        public class ShortestPathController : BaseApiController
+        [HttpPost("EightQueens", Name = "SaveEightQueensAnswer")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<OkObjectResult> SaveEightQueensAnswer([FromBody] EightQueensCommand command)
         {
-            [HttpPost("EightQueens", Name = "SaveEightQueensAnswer")]
-            [ProducesResponseType(StatusCodes.Status201Created)]
-            [ProducesResponseType(StatusCodes.Status400BadRequest)]
-            public async Task<OkObjectResult> SaveEightQueensAnswer([FromBody] EightQueensCommand command)
-            {
-                return Ok(await Mediator.Send(command));
-            }
-
+            return Ok(await Mediator.Send(command));
         }
+
+        [HttpGet("EightQueens", Name = "GetAllEightQueensSolution")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> GetAllEightQueensSolution()
+        {
+            var result = await Mediator.Send(new GetAllEightQueensDetailQuery());
+            return Ok(result);
+        }
+
     }
 }
